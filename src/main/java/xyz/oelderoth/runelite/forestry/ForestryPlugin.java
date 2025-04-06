b41c827836f5a0f77a3f8fe8fc42fa8349696b22
Oelderoth <gmzues@gmail.com>
2025-04-06 15:52:14 -0500

    Refactor main plugin to Java

---
+++ src/main/java/xyz/oelderoth/runelite/forestry/ForestryPlugin.java
@@ -0,0 +1,40 @@
+package xyz.oelderoth.runelite.forestry;
+
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+import net.runelite.client.ui.overlay.OverlayManager;
+import xyz.oelderoth.runelite.forestry.overlay.ForceSpawnOverlay;
+import xyz.oelderoth.runelite.forestry.service.ForceSpawnService;
+import javax.inject.Inject;
+
+@PluginDescriptor(
+    name = "Forestry Spawn Helper",
+    description = "A plugin to help with force spawning Forestry events"
+)
+@Slf4j
+public class ForestryPlugin extends Plugin {
+	static final String PLUGIN_NAME = "Forestry Spawn Helper";
+
+    @Inject
+    private ForceSpawnService forceSpawnService;
+
+    @Inject
+    private ForceSpawnOverlay forceSpawnOverlay;
+
+    @Inject
+    private OverlayManager overlayManager;
+
+	@Override
+	protected void startUp() throws Exception {
+		log.info("Starting Forestry Plugin");
+        forceSpawnService.enable();
+        overlayManager.add(forceSpawnOverlay);
+    }
+
+	@Override
+    public void shutDown() {
+        forceSpawnService.disable();
+        overlayManager.remove(forceSpawnOverlay);
+    }
+}
